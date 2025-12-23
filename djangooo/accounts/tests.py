390aa1ef4620e3c9f23b7d1c1e638b2a841c89d2
hedishafi <hafsahussein1221@gmail.com>
2025-12-23 04:15:42 -0800

    indentation error fix

--- djangooo/accounts/tests.py
+++ djangooo/accounts/tests.py
@@ -13,9 +13,10 @@ class AccountsViewTests(TestCase):
         }
 
     def test_signup_view(self):
-    response = self.client.post(reverse('accounts:signup'), self.user_data)
-    self.assertEqual(response.status_code, 302)  # redirect after signup
-    self.assertTrue(User.objects.filter(username='testuser').exists())
+        response = self.client.post(reverse('accounts:signup'), self.user_data)
+        self.assertEqual(response.status_code, 302)  # redirect after signup
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
     def test_login_view(self):
         User.objects.create_user(username='loginuser', password='Testpass123!')
         login_data = {'username': 'loginuser', 'password': 'Testpass123!'}
