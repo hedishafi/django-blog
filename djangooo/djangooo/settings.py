fa2b990ba9ea5bf5b7ece8184f0bcc7c2f73abf8
hedishafi <hafsahussein1221@gmail.com>
2025-12-23 06:29:38 -0800

    setting

--- djangooo/djangooo/settings.py
+++ djangooo/djangooo/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'djangooo.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],  
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -69,6 +69,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'djangooo.wsgi.application'
 
