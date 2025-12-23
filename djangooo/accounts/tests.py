6d6897ab6cd92d21c11b7e8460d526b9a9c207bb
hedishafi <hafsahussein1221@gmail.com>
2025-12-23 00:03:42 -0800

    Add Django tests and update CI/CD workflow

--- djangooo/accounts/tests.py
+++ djangooo/accounts/tests.py
@@ -1,3 +1,30 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+class AccountsViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user_data = {
+            'username': 'testuser',
+            'password1': 'Testpass123!',
+            'password2': 'Testpass123!'
+        }
+
+    def test_signup_view(self):
+        response = self.client.post(reverse('accounts:signup'), self.user_data)
+        self.assertEqual(response.status_code, 302)  # redirect after signup
+        self.assertTrue(User.objects.filter(username='testuser').exists())
+
+    def test_login_view(self):
+        User.objects.create_user(username='loginuser', password='Testpass123!')
+        login_data = {'username': 'loginuser', 'password': 'Testpass123!'}
+        response = self.client.post(reverse('accounts:login'), login_data)
+        self.assertEqual(response.status_code, 302)  # redirect after login
+
+    def test_logout_view(self):
+        user = User.objects.create_user(username='logoutuser', password='Testpass123!')
+        self.client.login(username='logoutuser', password='Testpass123!')
+        response = self.client.post(reverse('accounts:logout'))
+        self.assertEqual(response.status_code, 302)  # redirect after logout
--- djangooo/articles/tests.py
+++ djangooo/articles/tests.py
@@ -1,3 +1,43 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from .models import Article
+from django.urls import reverse
 
-# Create your tests here.
+class ArticleModelTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='author', password='Testpass123!')
+
+    def test_article_creation(self):
+        article = Article.objects.create(
+            title='Test Article',
+            slug='test-article',
+            body='This is the body of the test article.',
+            author=self.user
+        )
+        self.assertEqual(str(article), 'Test Article')
+        self.assertEqual(article.snippet(), 'This is the body of the test article....')
+
+class ArticleViewTests(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(username='author', password='Testpass123!')
+        self.article = Article.objects.create(
+            title='Test Article',
+            slug='test-article',
+            body='This is the body of the test article.',
+            author=self.user
+        )
+
+    def test_article_list_view(self):
+        response = self.client.get(reverse('articles:list'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_article_detail_view(self):
+        response = self.client.get(reverse('articles:detail', args=[self.article.slug]))
+        self.assertEqual(response.status_code, 200)
+
+    def test_article_create_view_requires_login(self):
+        response = self.client.get(reverse('articles:create'))
+        self.assertEqual(response.status_code, 302)  # redirect to login
